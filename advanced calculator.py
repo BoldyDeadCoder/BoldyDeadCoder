2ac624909811d713952d6e68fefaef9c64ed8f67
BoldyDeadCoder <62553730+BoldyDeadCoder@users.noreply.github.com>
2021-09-17 16:12:55 -0400

    Update advanced calculator.py
    V1.0.0-created project
    V1.0.1-fix a bug where the project didn't stop completely
    Update-1.0.2...
    by next week a update is going to be rolled out.
    will include new optimizations and a new syntax for the code.
    
    Update-1.0.3-includes new optimizations and a new syntax for the code.

--- advanced calculator.py
+++ advanced calculator.py
@@ -1,100 +1,39 @@
+import time
 import sys
-number = int(float(input('1.Type here: ')))
-# number is a variable that we are storing information.
-number1 = int(float(input('2.Type here: ')))
+Var1 = int(input('Type here: '))
+Var2 = int(input('Type here: '))
 # int() means a number or integer, input() means to get the users input.
-if number and number1 is float() or int():
-    # A float() represents a decimal number.
-    print(number + number1)
-elif number and number1 is float() or int():
-    print(number - number1)
-    # A Int() is a whole number.
-elif number and number1 is float() or int():
-    print(number * number1)
-    # A float() represents a decimal number.
-# A Int() is a whole number.
-# A float() represents a decimal number.
-# A Int() is a whole number.
-elif number and number1 is float() or int():
-    # A float() represents a decimal number.
-    print(number / number1)
-    # A Int() is a whole number.
+print("1.add 'add 'in the next line if you want to add")
+print("2.add 'minus' in the next line if you want to subtract")
+print("3.add 'multiply' in the next line if you want to multiply")
+print("4.add 'divide' if in the next line you want to divide")
+time.sleep(2)
 
-matter = int(float(input('3.Type here: ')))
-matter1 = int(float(input('4.Type here: ')))
-if matter and matter1 is float() or int():
-    print(matter + matter1)
-elif matter and matter1 is float() or int():
-    print(matter - matter1)
-elif matter and matter1 is float() or int():
-    print(matter * matter1)
-elif matter and matter1 is float() or int():
-    print(matter / matter1)
-# new means a variable called new, int() means a integer or number and not a string. A int() will only accept integers
-# and can't accept strings such as 'HI'.
-new = int(input('3.Type here: '))
-digit2 = int(input('4.Type here:'))
-if new and digit2 is float() or int():
-    print(new + digit2)
-elif new and digit2 is float() or int():
-    print(new - digit2)
-elif new and digit2 is float() or int():
-    print(new * digit2)
-elif new and digit2 is float() or int():
-    print(new / digit2)
+if Var1 == input():
+    print("Here you will put your equations in the calculator.")
+elif input("Type here 'add' if you want to automatically add the problem: ") == "add":
+    print(int(Var1 + Var2))
+elif input("Type here 'minus' if you want to automatically subtract the problem: ") == "minus":
+    print(int(Var1 - Var2))
+elif input("Type here 'multiply' if you want automatically to multiply the problem: ") == "multiply":
+    print(int(Var1 * Var2))
+elif input("Type here 'divide' if you want to automatically divide the problem: ") == "divide":
+    print(int(Var1 / Var2))
+print("if you want to add, multiply, divide, or subtract decimals then only type the decimal problem.")
+time.sleep(2)
 
-car = int(input('5.Type here: '))
-ride = int(input('6.Type here: '))
-if car and ride is float() or int():
-    print(car + ride)
-elif car and ride is float() or int():
-    print(car - ride)
-elif car and ride is float() or int():
-    print(car * ride)
-elif car and ride is float() or int():
-    print(car / ride)
-print('The next one is only for floats()')
+Var3 = float(input('3.Type here: '))
+Var4 = float(input('4.Type here: '))
 
-float_1 = float(input('7.Type here: '))
-# float_1 is a variable, a variable is a box that allows you to store things inside of and then execute later on
-# afterwards
-float_2 = float(input('8.Type here: '))
-if float_1 < float_2:
-    # This allows python to check if float_1 and float_1 are bigger than each other or less than each other and it would
-    # add or multiply or add and subtract, divide each number or variable.
-    print(float(float_1 + float(float_2)))
-elif float_1 > float_2:
-    # This allows python to check if float_1 and float_1 are bigger than each other or less than each other and it would
-    #  add or multiply or add and subtract, divide each number or variable.
-    print(float(float_1 + float(float_2)))
-elif float_1 < float_2:
-    # This allows python to check if float_1 and float_1 are bigger than each other or less than each other and it would
-    # add or multiply or add and subtract, divide each number or variable.
-    print(float(float_1 - float(float_2)))
-elif float_1 > float_2:
-    # This allows python to check if float_1 and float_1 are bigger than each other or less than each other and it would
-    # add or multiply or add and subtract, divide each number or variable.
-    print(float(float_1 - float(float_2)))
-elif float_1 < float_2:
-    # This allows python to check if float_1 and float_1 are bigger than each other or less than each other and it would
-    # add or multiply or add and subtract, divide each number or variable.
-    print(float(float_1 * float(float_2)))
-elif float_1 > float_2:
-    # This allows python to check if float_1 and float_1 are bigger than each other or less than each other and it would
-    # add or multiply or add and subtract, divide each number or variable.
-    print(float(float_1 * float(float_2)))
-elif float_1 < float_2:
-    # This allows python to check if float_1 and float_1 are bigger than each other or less than each other and it would
-    # add or multiply or add and subtract, divide each number or variable.
-    print(float(float_1 / float(float_2)))
-elif float_1 > float_2:
-    # This allows python to check if float_1 and float_1 are bigger than each other or less than each other and it would
-    # add or multiply or add and subtract, divide each number or variable.
-    print(float(float_1 / float(float_2)))
-elif float(float_1 and float(float_2 == float(float_1 and float(float_2)))):
-    # This line allows python3.9 to check if both variables = the same numbers and if they do then it will say
-    # "This are the same."
-    print("This numbers are the same")
+if Var3 == input():
+    print("Here you will put your equations in the calculator.")
+elif input("Type here 'add' if you want to automatically add the problem: ") == "add":
+    print(float(Var3 + Var4))
+elif input("Type here 'subtract' if you want to automatically subtract the problem: ") == "subtract":
+    print(float(Var3 - Var4))
+elif input("Type here 'multiply' if you want to automatically multiply the problem: ") == "multiply":
+    print(float(Var3 * Var4))
+elif input("Type here 'divide' if you want to automatically divide the problem: ") == "divide":
+    print(float(Var3 / Var4))
 print("end of program" + "Goodbye")
 sys.exit()
-

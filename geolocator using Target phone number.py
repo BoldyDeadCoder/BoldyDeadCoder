1da619debb0d6a0c025a343ac47fc175a116ea2b
BoldyDeadCoder <62553730+BoldyDeadCoder@users.noreply.github.com>
2023-12-05 12:21:30 -0800

    Update geolocator using Target phone number.py
    12/5/2023
    
    Updated source code with newer code.

--- geolocator using Target phone number.py
+++ geolocator using Target phone number.py
@@ -2,33 +2,27 @@ import phonenumbers
 from phonenumbers import timezone
 from phonenumbers import geocoder, carrier
 from phonenumbers import is_carrier_specific_for_region
-import time
-print("You need to enter the Target area code")
-
-time.sleep(2)
 
 phoneNumber = phonenumbers.parse(input("Type here the Target phone number: "))
 
 timeZone = timezone.time_zones_for_number(phoneNumber)
 
-choose_language1 = print("Format has to be 'en'")
+Carrier = carrier.name_for_number(phoneNumber)
 
-choose_language = input("Type here the language you want: ")
+Carrier_is_specific_for_carrier_region = is_carrier_specific_for_region(.parse."US",(phoneNumber)
 
-Carrier = carrier.name_for_number(phoneNumber, choose_language)
+Region = geocoder.description_for_number(phoneNumber)
 
-Carrier_is_specific_for_carrier_region = is_carrier_specific_for_region(phoneNumber, choose_language)
+if phoneNumber.is_valid_number(phoneNumber):
+  print(timeZone, Region,)
+  print(Carrier_is_specific_for_carrier_region)
+  
+elif phoneNumber.is_invalid_number(phoneNumber):
+  phoneNumber = phonenumbers.parse(input("Type here the Target phone number: "))
+  print(timeZone, Region,)
+  print(Carrier_is_specific_for_carrier_region)
+  print(phoneNumber.is_valid_number(phoneNumber))
 
-Region = geocoder.description_for_number(phoneNumber, choose_language)
+print(phoneNumber.is_valid_number(phoneNumber)
 
-valid = phonenumbers.is_valid_number(phoneNumber)
 
-possible = phonenumbers.is_possible_number(phoneNumber)
-
-print(timeZone)
-print(Carrier)
-print(phoneNumber)
-print(Region)
-print(valid)
-print(possible)
-pass

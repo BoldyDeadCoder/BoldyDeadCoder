c72d160996aea1252975edc0181f3b51b9926735
BoldyDeadCoder <62553730+BoldyDeadCoder@users.noreply.github.com>
2024-01-30 21:46:00 -0500

    Update advanced calculator.py

--- advanced calculator.py
+++ advanced calculator.py
@@ -1,25 +1,24 @@
-Var1 = int(input('Type here: '))
-Var2 = int(input('Type here: '))
-# int() means a number or integer, input() means to get the users input.
-adding = input("Type here '+, -, *, /': ")
-if adding == '+':
-    print(Var1 + Var2)
-if adding == '-':
-    print(Var1 - Var2)
-if adding == '*':
-    print(Var1 * Var2)
-if adding == '/':
-    print(Var1 / Var2)
+def calculate():
+    try:
+        Var1 = int(input('Type here: '))
+        Var2 = int(input('Type here: '))
+        operation = input("Type here '+, -, *, /': ")
+        if operation == '+':
+            print(Var1 + Var2)
+        elif operation == '-':
+            print(Var1 - Var2)
+        elif operation == '*':
+            print(Var1 * Var2)
+        elif operation == '/':
+            if Var2 != 0:
+                print(Var1 / Var2)
+            else:
+                print("Error: Division by zero is not allowed.")
+        else:
+            print("Error: Invalid operator. Please enter '+, -, *, /'.")
+    except ValueError:
+        print("Error: Invalid input. Please enter a number.")
 
-Bar1 = int(input('Type here: '))
-Bar2 = int(input('Type here: '))
-# int() means a number or integer, input() means to get the users input.
-minting = input("Type here '+, -, *, /': ")
-if minting == '+':
-    print(Bar1 + Bar2)
-if minting == '-':
-    print(Bar1 - Bar2)
-if minting == '*':
-    print(Bar1 * Bar2)
-if minting == '/':
-    print(Bar1 / Bar2)
+# Call the function twice to replicate your original code
+calculate()
+calculate()

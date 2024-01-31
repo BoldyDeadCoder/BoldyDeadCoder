f5de019b01f370d749f9ff0e47bb67d85da9dfca
BoldyDeadCoder <62553730+BoldyDeadCoder@users.noreply.github.com>
2024-01-30 22:23:10 -0500

    Update geolocator using Target phone number.py

--- geolocator using Target phone number.py
+++ geolocator using Target phone number.py
@@ -1,28 +1,19 @@
 import phonenumbers
 from phonenumbers import timezone
 from phonenumbers import geocoder, carrier
-from phonenumbers import is_carrier_specific_for_region
+from phonenumbers import is_valid_number
 
 phoneNumber = phonenumbers.parse(input("Type here the Target phone number: "))
 
 timeZone = timezone.time_zones_for_number(phoneNumber)
 
-Carrier = carrier.name_for_number(phoneNumber)
+Carrier = carrier.name_for_number(phoneNumber, 'en')
 
-Carrier_is_specific_for_carrier_region = is_carrier_specific_for_region(.parse."US",(phoneNumber)
+Region = geocoder.description_for_number(phoneNumber, 'en')
 
-Region = geocoder.description_for_number(phoneNumber)
-
-if phoneNumber.is_valid_number(phoneNumber):
-  print(timeZone, Region,)
-  print(Carrier_is_specific_for_carrier_region)
+if is_valid_number(phoneNumber):
+  print(timeZone, Region)
+  print(Carrier)
   
-elif phoneNumber.is_invalid_number(phoneNumber):
-  phoneNumber = phonenumbers.parse(input("Type here the Target phone number: "))
-  print(timeZone, Region,)
-  print(Carrier_is_specific_for_carrier_region)
-  print(phoneNumber.is_valid_number(phoneNumber))
-
-print(phoneNumber.is_valid_number(phoneNumber)
-
-
+else:
+  print("Invalid phone number")

